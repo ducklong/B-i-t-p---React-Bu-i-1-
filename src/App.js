b3eb8cfbab22ed7b8d5df38234f3d61d990f9760
ducklong <longmkting@gmail.com>
2022-05-15 09:46:45 -0700

    nop bai

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+// import 'jquery/dist/jquery.min.css';
+import Header from "./Header/Header";
+import Body from "./Body/Body";
+import Fotter from "./Fotter/Fotter";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Fotter/>
+    </>
+  );
+}
+
+export default App;
